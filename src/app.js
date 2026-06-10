61126767f1c3726cf68b8dd5500ddf5339284d0f
PRAKHAR-PANDEY99 <prakharpandey1309@gmail.com>
2026-06-10 21:34:48 +0530

    exploring get post delete

--- src/app.js
+++ src/app.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const app = express();
-app.use("/test", (req, res) => {
+app.get('/test', (req, res) => {
+    res.send('Hello World!');
+}
+);
+app.post('/test', (req, res) => {
+    res.send('Hello World!');
+});
+app.use("/tests", (req, res) => {
     res.send("Hello World hi man ");
+});
+app.delete('/testdelete', (req, res) => {
+    res.send('Hello World! Delete');
 });
 app.listen(3000, () => {console.log('Server is running on port 3000');   
 });
