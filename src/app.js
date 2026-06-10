0a6f66a1b2fa75ac828fcbd024ca239d46ab673a
PRAKHAR-PANDEY99 <prakharpandey1309@gmail.com>
2026-06-11 02:46:14 +0530

    init database

--- src/app.js
+++ src/app.js
@@ -1,18 +1,31 @@
 const express = require('express');
+const connectDB =require('./config/database');
 const app = express();
-const adminAuth = require('./middlewares/auth');
-app.get('/test', (req, res) => {
-    res.send('Hello World!');
-}
-);
-app.use("/admin",adminAuth);
-app.get('/admin/dashboard', (req, res) => {
-    res.send('Welcome to the admin dashboard!');
+const User = require('./models/user');
+app.post("/signup", async (req, res) => {
+    const userobj={
+        firstName: "Prakhar",
+        lastName: "Pandey",
+        emailID: "pandey.prakhar@example.com",
+        password: "password123",
+        age: 25,
+        gender: "Male"
+    }
+    // creating a new instance of the User model with the user data
+    const user = new User(userobj);
+    try {        await user.save();
+        res.send({message:"User created successfully"});
+    } catch (error) {
+        console.error("Error creating user", error);    
+        res.status(500).send({message:"Error creating user"});
+    }
 });
-app.get('/admin/settings', (req, res) => {
-    res.send('Welcome to the admin settings!');
+connectDB().then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(3000, () => {console.log('Server is running on port 3000');   
+});
+}).catch((err) => { 
+    console.error("Error connecting to MongoDB", err);
 });
 
-app.listen(3000, () => {console.log('Server is running on port 3000');   
-});
   
