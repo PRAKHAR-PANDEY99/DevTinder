38706853ade3a3c52c92e454d74b74edea431a82
PRAKHAR-PANDEY99 <prakharpandey1309@gmail.com>
2026-07-01 21:44:50 +0530

    database Edit

--- src/config/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const connectDB = async () => {
-    await mongoose.connect("mongodb://PrakharPandey:PakharPandey31@ac-w8e6jsd-shard-00-00.qjcbvbz.mongodb.net:27017,ac-w8e6jsd-shard-00-01.qjcbvbz.mongodb.net:27017,ac-w8e6jsd-shard-00-02.qjcbvbz.mongodb.net:27017/devTinder?ssl=true&replicaSet=atlas-kmdxnh-shard-0&authSource=admin&appName=PakharNode/devTinder",)
-};
-module.exports = connectDB;
-
-
- 
